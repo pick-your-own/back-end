b6cd3c32321ac9b2b473369c271db06a4d6edd67
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-06-14 11:14:47 -0500

    idk

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 require('dotenv').config();
 
 const { Server } = require('socket.io');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const { UserQueue } = require('./src/lib/UserQueue');
 const { eventPool } = require('./eventPool');
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/chatApp';
+const PORT = process.env.PORT || 3002;
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/chatApp';
 
 const io = new Server(PORT, {
   cors: {
@@ -18,17 +18,19 @@ const io = new Server(PORT, {
   },
 });
 
+console.log('listening on port', PORT);
+
 let UserQ = new UserQueue();
 
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
+// mongoose
+//   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((error) => console.error('MongoDB connection error:', error));
 
 io.on('connection', (socket) => {
   socket.on(eventPool.USER_JOIN, user => {
     console.log('connected');
-
+    
   });
 
   socket.on('disconnect', () => {
